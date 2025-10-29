b9a99b5f16bc6b42b559886fea6536b7684d7a5e
Jasmine <jayjadesanders@gmail.com>
2025-10-29 12:08:59 -0400

    Basic home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,138 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { FlatList, Image, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+
+//Create Recipe Type
+interface Recipe {
+  id: number,
+  title: string,
+  image: string,
+  imageType: string
+}
+
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  // TEMP
+  const APIKEY = '68ca40677c7b4d3fa5fd36d44dd636a6'
+  const router = useRouter()
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const [searchText, setSearchText] = useState<string>("");
+  const [searchedRecipes, setSearchedRecipes] = useState<Recipe[]>([])
+  const [favoriteRecipes, setFavoriteRecipes] = useState<Recipe[]>([])
+
+  function handleSearchText(text: string) {
+    setSearchText(text)
+  }
+
+  async function handleSearch(){
+    console.log('searching...')
+    try {
+      const res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchText}&apiKey=${APIKEY}`)
+      if(!res.ok){
+        throw new Error(`Error fetching Recipes ${res.status}`)
+      }
+      const recipes = await res.json()
+      console.log(recipes)
+      console.log(recipes.results)
+      setSearchedRecipes(recipes.results)
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  const toggleFavorite = (recipe: Recipe, value: boolean) => {
+    if (value){
+      setFavoriteRecipes([...favoriteRecipes, recipe])
+    }else{
+      setFavoriteRecipes(favoriteRecipes.filter((r) => r.id !== recipe.id))
+    }
+    console.log(favoriteRecipes)
+  }
+
+  const isFavorite = (recipe: Recipe) => {
+    return favoriteRecipes.some((r) => r.id === recipe.id)
+  }
+
+
+  return (
+   <View style={styles.container}>
+      <Text style={styles.title}>Recipe Finder</Text>
+      <TextInput
+        style={styles.searchBar}
+        placeholder='Search...'
+        value={searchText}
+        onChangeText={handleSearchText}
+      />
+      <TouchableOpacity
+        style={styles.searchButton}
+        onPress={handleSearch}
+      >
+        <Text>Search</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        data={searchedRecipes}
+        keyExtractor={(recipe) => recipe.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Switch
+              value={isFavorite(item)}
+              onValueChange={(value) => toggleFavorite(item, value)}
+            />
+            <Image
+              source={{uri: item.image}}
+              style={styles.recipeImage}
+            />
+
+            <TouchableOpacity
+              style={styles.searchButton}
+              onPress={() => router.push(`/recipe?recipeId=${item.id.toString()}`)}
+            >
+              <Text>See Recipe</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    padding: 20,
+    gap: 10
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title:{
+    fontSize: 20,
+    fontWeight: '400'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  searchBar: {
+    height: 40,
+    borderColor: 'lightblue',
+    borderWidth: 3,
+    borderRadius: 10,
+    padding: 2,
+    width: '100%'
   },
-});
+  searchButton: {
+    backgroundColor: 'lightblue',
+    padding: 5,
+    borderRadius: 5,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  recipeImage: {
+    width: 100,
+    height: 100
+  }
+})
+
+
+
